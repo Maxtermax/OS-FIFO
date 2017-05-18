e837ab53cf303b1dfcb218e788882ff631668958
maxtermax <esnene02@gmail.com>
2017-05-18 15:09:04 -0500

    icons served ?

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const server = require("http").createServer(app);
+
+app.use(express.static(__dirname+"/build"));
+
+app.get('/',function (req,res){
+ res.sendFile(__dirname+'/index.html','utf8');
+})
+
+
+app.listen("3000",()=> {
+  console.log("listen in port: http://localhost:3000/")
+})
